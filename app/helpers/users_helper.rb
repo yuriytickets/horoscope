b0c2e84fe82c6eeea0c3b0ce8a92664a88acfee9
maxsuhak <maxsuhak@gmail.com>
2013-10-22 17:21:51 +0300

    added spec, fix zodiac img

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,29 +7,29 @@ module UsersHelper
 	def zodiac_avatar(zodiac)
 		case zodiac
 			when "Taurus"
-				image_tag 'zodiac/телец.jpg'
+				image_tag 'zodiac/Taurus.jpg'
 			when "Gemini"
-				image_tag 'zodiac/близнецы.jpg'
+				image_tag 'zodiac/Gemini.jpg'
 			when "Libra"
-				image_tag 'zodiac/весы.jpg'
+				image_tag 'zodiac/Libra.jpg'
 			when "Aquarius"
-				image_tag 'zodiac/водолей.jpg'
+				image_tag 'zodiac/Aquarius.jpg'
 			when "Virgo"
-				image_tag 'zodiac/дева.jpg'
+				image_tag 'zodiac/Virgo.jpg'
 			when "Capricorn"
-				image_tag 'zodiac/козерог.jpg'
+				image_tag 'zodiac/Capricorn.jpg'
 			when "Leo"
-				image_tag 'zodiac/лев.jpg'
+				image_tag 'zodiac/Leo.jpg'
 			when "Aries"
-				image_tag 'zodiac/овен.jpg'
+				image_tag 'zodiac/Aries.jpg'
 			when "Cancer"
-				image_tag 'zodiac/рак.jpg'
+				image_tag 'zodiac/Cancer.jpg'
 			when "Pisces"
-				image_tag 'zodiac/рыба.jpg'
+				image_tag 'zodiac/Pisces.jpg'
 			when "Scorpio"
-				image_tag 'zodiac/скорпион.jpg'
+				image_tag 'zodiac/Scorpio.jpg'
 			when "Sagittarius"
-				image_tag 'zodiac/стрелец.jpg'
+				image_tag 'zodiac/Sagittarius.jpg'
 			else
 				image_tag 'zodiac/troll.jpg'
 		end
